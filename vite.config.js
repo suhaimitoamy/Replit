d249bb705803ac2dbcaababe7a6a8d24da3917ea
suhaimitoamy <suhaimitoamy@gmail.com>
2026-05-27 10:01:41 +0800

    Update base path for Replit Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Amy-Trading-Quest/', // Useful for GitHub Pages
+  base: '/Replit/',
   server: {
     port: 3000,
   },
